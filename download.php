f27c74c8051a0a9cd305f8f5ce3d9c945daf230d
Unknown <D252023@edu.rocwb.nl>
2018-03-13 15:15:34 +0100

    Added database

--- download.php
+++ download.php
@@ -6,14 +6,16 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Download De Gokkers</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Download</h1>
-
-    <video width="500" controls>
-        <source src="input video name" type="video/mp4">
-
-        Your browser does not support HTML5 video.
+<div class="video">
+    <video width="500" height="400" controls>
+        <source src="img/trailer.mp4" type="video/mp4">
+        <source src="movie.ogg" type="video/ogg">
+        Your browser does not support the video tag.
     </video>
+</div>
 </body>
 </html>
