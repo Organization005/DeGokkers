4b6d0f5f2c3f056c46871aea74794b2c2206ffd7
Unknown <d252874@edu.rocwb.nl>
2018-03-13 16:48:06 +0100

    added min lenght password

--- validate.php
+++ validate.php
@@ -4,26 +4,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($_POST['password'] !== ' ') {
         //two passwords are equal to each other
         if ($_POST['password'] === $_POST['confirmpassword']) {
+            if ($_POST['password'] > 7){
+
+                //define other variables with submitted values from $_POST
+                $email = $mysqli->real_escape_string($_POST['email']);
+
+                //md5 hash password for security
+                $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 
-            //define other variables with submitted values from $_POST
-            $email = $mysqli->real_escape_string($_POST['email']);
+                //insert user data into database
+                $sql = "INSERT INTO users (email, password) "
+                    . "VALUES ('$email', '$hashed_password')";
 
-            //md5 hash password for security
-            $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                //check if mysql query is successful
+                if ($mysqli->query($sql) === true) {
+                    $_SESSION['message'] = "Registration succesful!";
+                } else {
+                    $_SESSION['message'] = 'User could not be added to the database!';
+                }
+                $mysqli->close();
 
-
-            //insert user data into database
-            $sql = "INSERT INTO users (email, password) "
-                . "VALUES ('$email', '$hashed_password')";
-
-            //check if mysql query is successful
-            if ($mysqli->query($sql) === true) {
-                $_SESSION['message'] = "Registration succesful!";
-            } else {
-                $_SESSION['message'] = 'User could not be added to the database!';
             }
-            $mysqli->close();
+            else {
+                $_SESSION['message'] = "you password needs to be longer than 7 characters";
+            }
+
+
 
 
         }
