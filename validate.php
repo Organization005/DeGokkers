c53b7e304f97568eef295cf2344ae96c701b4e2d
Unknown <d252874@edu.rocwb.nl>
2018-03-19 14:10:39 +0100

    change form mysqli to PDO

--- validate.php
+++ validate.php
@@ -4,25 +4,43 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($_POST['password'] !== ' ') {
         //two passwords are equal to each other
         if ($_POST['password'] === $_POST['confirmpassword']) {
-            if ($_POST['password'] > 7){
+
+            $passwordlenght = strlen($_POST['password']);
+            if ($passwordlenght > 7){
 
                 //define other variables with submitted values from $_POST
                 $email = $mysqli->real_escape_string($_POST['email']);
 
                 $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $dsn = 'mysql:accounts=users;host=127.0.0.1';
+                $user = 'root';
+                $password = '';
+
+                try{
+                    $dbh = new PDO($dsn, $user , $password);
+                }catch (PDOException $e){
+                    echo 'Connection failed:' . $e->getMessage;
+                }
 
 
                 //insert user data into database
-                $sql = "INSERT INTO users (email, password) "
+               $sql = "INSERT INTO users (email, password) "
                     . "VALUES ('$email', '$hashed_password')";
 
-                //check if mysql query is successful
                 if ($mysqli->query($sql) === true) {
                     $_SESSION['message'] = "Registration succesful!";
                 } else {
                     $_SESSION['message'] = 'User could not be added to the database!';
                 }
-                $mysqli->close();
+
+//
+//                //check if mysql query is successful
+//                if ($mysqli->query($sql) === true) {
+//                    $_SESSION['message'] = "Registration succesful!";
+//                } else {
+//                    $_SESSION['message'] = 'User could not be added to the database!';
+//                }
+//                $mysqli->close();
 
             }
             else {
