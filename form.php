4220a565b59935ea506283cfc4e1615f90c36554
Unknown <D252023@edu.rocwb.nl>
2018-03-20 15:02:15 +0100

    Style

--- form.php
+++ form.php
@@ -19,8 +19,16 @@ require 'validate.php';
 </head>
 <body>
 
-<h1>register</h1>
-<div class="register">
+<header>
+<button id="myBtn">Register</button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+    <!-- Modal content -->
+    <div class="modal-content">
+        <span class="close">&times;</span>
+            <div class="register">
 
     <form class="form" action="form.php" method="post" enctype="multipart/form-data" autocomplete="off">
         <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
@@ -30,10 +38,11 @@ require 'validate.php';
         <div class="submit">
             <input type="submit" value="register" name="register" class="btn btn-block btn-primary" />
         </div>
+        </div>
+    </div>
     </form>
 </div>
 
-<h1>login</h1>
 <div class="login">
 
     <form class="form" action="form.php" method="post" enctype="multipart/form-data" autocomplete="off">
@@ -45,6 +54,34 @@ require 'validate.php';
         </div>
     </form>
 </div>
+<script>
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>
+</header>
 </body>
 </html>
 
