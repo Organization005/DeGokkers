7337f2e25ef57ecad1754a69ca15fdb110e01b65
Unknown <D252023@edu.rocwb.nl>
2018-03-20 16:51:58 +0100

    Auto stash before merge of "master" and "879tr67tf7u/master"

--- validate.php
+++ validate.php
@@ -60,44 +60,39 @@ if(isset($_POST['register'])) {
                 $_SESSION['message'] = 'fill in a password';
             }
         }
-        if ($login == false ){
+        if ($login == false) {
             $logemail = $_POST['logemail'];
             $logpassword = $_POST['logpassword'];
             $logincheck = false;
             $loogpassword = ' ';
 
             $query = $dbh->prepare("SELECT email FROM users WHERE email = :email");
-            $query->bindValue( ":email", $logemail);
+            $query->bindValue(":email", $logemail);
             $query->execute();
 
 
             if ($query->rowCount() > 0) {
 
-                $query = $dbh->prepare("SELECT password FROM users where email = :email");
-                $query->bindValue( ":email", $logemail);
+                $query = $dbh->prepare("SELECT password FROM users WHERE email = :email");
+                $query->bindValue(":email", $logemail);
                 $hash = $query->execute();
 
-                if (password_verify($logpassword, $hash)){
-                    $logincheck   = true;
+                if (password_verify($logpassword, $hash)) {
+                    $logincheck = true;
                 }
-            }else{
+            } else {
                 $logincheck = false;
 
             }
 
-            if ($logincheck === false){
+            if ($logincheck === false) {
                 $_SESSION['logmessage'] = "your password or email is not correct";
 
 
-            }else{
+            } else {
                 $_SESSION['logmessage'] = "you are login now";
             }
         }
     }
 
-    }
-    
-
-
-
-
+}
